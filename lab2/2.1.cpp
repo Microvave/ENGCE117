e7691f40470bc13d8e9041680af19ff410ca5a5d
Microvave <118260391+Microvave@users.noreply.github.com>
2023-01-07 23:19:02 +0700

    Add files via upload

---
+++ lab2/2.1.cpp
@@ -0,0 +1,11 @@
+#include <stdio.h>
+#include <string.h>
+
+int main() {
+
+    char text[10];
+    strcpy(text, "Microave");
+    printf("%s", text);
+    return 0;
+    
+}
---
+++ lab2/2.4.cpp
@@ -0,0 +1,15 @@
+#include <stdio.h>
+
+void GetText( char **text ); 
+int main(){
+    char *str;
+    GetText(&str);
+    printf("%s", str);
+    return 0;
+}
+
+void GetText( char **text ) { 
+    *text = new char[20];
+    printf("Enter your text : ");
+    gets(*text); 
+}
